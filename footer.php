86bb03cfcae3857a98bf9ce46318126b9b83cef1
Emmanuel Thomas <92178178+Thomas21Emmanuel@users.noreply.github.com>
2022-02-11 21:13:53 -0500

    Modification du footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer class="site__footer">
-    <h2 class="footer__titre">Le footer</h2>
+    <h2 class="footer__titre">Le footer d'Emmanuel Thomas</h2>
     <p class="footer__presentation">Le site web de l'exercise 1</p>
     <?php wp_nav_menu(array("menu"=>"footer",
                             "container"=>"nav",
