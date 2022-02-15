a5ebd952fd8c49290bf59cd0596ecca9077a7582
Emmanuel Thomas <e1870284@cmaisonneuve.qc.ca>
2022-02-15 10:52:36 -0500

    Notre premier commit

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer class="site__footer">
+    <h2 class="footer__titre">Le footer d'Emmanuel Thomas</h2>
+    <p class="footer__presentation">Le site web de l'exercise 1</p>
+    <?php wp_nav_menu(array("menu"=>"simple",
+                            "container"=>"nav",
+                            "container_class"=>"footer")); ?>
+</footer>
+</body>
+
+</html>
