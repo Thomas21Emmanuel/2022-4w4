27ce29b18188c1b2bda698c1a8325c15c0c235ee
Emmanuel Thomas <92178178+Thomas21Emmanuel@users.noreply.github.com>
2022-02-28 16:25:27 -0500

    Mis à jour front-page.php

--- front-page.php
+++ front-page.php
@@ -9,7 +9,6 @@
 <div class="animation__bloc">5</div>
 
 </section>      
-<h1>front</h1>
             <?php if (have_posts()): while(have_posts()) : the_post(); ?>
             <?php the_title() ?>
             <?php the_content() ?>
