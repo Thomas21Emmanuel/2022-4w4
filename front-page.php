cbd4b05f9a1375ba338805d9b48700bd32ba1082
Emmanuel Thomas <thomas21emmanuel@gmail.com>
2022-03-09 14:10:43 -0500

    Commit des changements de couleurs avec le mixin et changement a l'acceuil

--- front-page.php
+++ front-page.php
@@ -2,11 +2,11 @@
 <main class="site__main">
 
 <section class="animation">
-<div class="animation__bloc">1</div>
-<div class="animation__bloc">2</div>
-<div class="animation__bloc">3</div>
-<div class="animation__bloc">4</div>
-<div class="animation__bloc">5</div>
+<div class="animation__bloc">Dévelopement de jeux</div>
+<div class="animation__bloc">Infographie</div>
+<div class="animation__bloc">Dévelopement Web</div>
+<div class="animation__bloc">Montage vidéo</div>
+<div class="animation__bloc">Création 3D</div>
 
 </section>      
             <?php if (have_posts()): while(have_posts()) : the_post(); ?>
