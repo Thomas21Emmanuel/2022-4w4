20461cd1deda7851a24cf4e454d82c72f2626269
Emmanuel Thomas <e1870284@cmaisonneuve.qc.ca>
2022-05-05 11:41:43 -0400

    s13 c2 Debut caroussel

--- functions.php
+++ functions.php
@@ -5,11 +5,19 @@ require_once("options/apparence.php");
 function cidw_4w4_enqueue(){
     wp_enqueue_style('cidw-4w4-le-style', get_template_directory_uri() . '/style.css', array(), filemtime(get_template_directory() . '/style.css'), false);
     wp_enqueue_style('cidw-4w4-police-google', "https://fonts.googleapis.com/css2?family=Nunito:wght@300&family=PT+Serif:ital@1&family=Roboto+Condensed&display=swap", false);
+    
     wp_enqueue_script('cidw-4w4-js-modal',
      get_template_directory_uri() . '/javascript/boite_modale.js',
       array(), '1.0.0',
       filemtime(get_template_directory() . '/javascript/boite_modale.js'),
      true );
+
+     wp_enqueue_script('cidw-4w4-caroussel',
+     get_template_directory_uri() . '/javascript/caroussel.js',
+      array(), '1.0.0',
+      filemtime(get_template_directory() . '/javascript/caroussel.js'),
+     true );
+    
     
 }
 
