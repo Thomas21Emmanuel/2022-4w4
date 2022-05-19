42d016940d48f7a0c0d49924dfdcbd74e08d1ef6
Emmanuel Thomas <e1870284@cmaisonneuve.qc.ca>
2022-05-19 13:24:24 -0400

    s16 ef Formattage HTML du gabarit des ateliers

--- sass/template-atelier.php
+++ sass/template-atelier.php
@@ -3,20 +3,52 @@
 <main class="site__main">
     <article class="atelier"></article>
 <?php if (have_posts()): the_post(); ?>
+<h1 class="atelier__titre">
+    <?php the_field('titre');?>
+</h1>
+<h2>
+    Description de l'atelier
+</h2>
 <section class="atelier__animateur">
+    <p>L'animateur de l'atelier :
     <?php the_field('animateur');?>
+</p>
     </section>
     <p class="atelier__local">
+        <p>L'atelier sera donné au local:
     <?php the_field('local');?>
 </p>
-<p class="evenement__date">
-    <?php the_field('date');?>
 </p>
-<p class="evenement__heure">
-    <?php the_field('heure');?>
+<p class="atelier__description">
+    <?php the_field('description');?>
 </p>
-<p class="evenement__organisateur">
-    <?php the_field('organisateur');?>
+<h2>Horaire et date de l'atelier</h2>
+<p class="atelier__duree">
+    <p>La durée de chacune des séances est de:
+    <?php the_field('duree');?>
+    heures
+</p>
+</p>
+<p class="atelier__date__debut">
+    <p>Date de début:
+    <?php the_field('date_de_debut');?>
+</p>
+</p>
+<p class="atelier__date__fin">
+    <p>Date de fin :
+    <?php the_field('date_de_fin');?>
+</p>
+</p>
+<p class="atelier_journee">
+    <p>La formation se donnera :
+    <?php the_field('journee');?>
+    </p>
+</p>
+<p class="atelier__heure">
+    <p>L'horaire : De
+    <?php the_field('heure_de_debut');?>
+    à
+    <?php the_field('heure_de_fin');?>
 </p>
 </article>
 <?php endif ?>
